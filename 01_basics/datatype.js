966ae10e7087a752e6cebb146a75263bf6c617be
E.R S.K YADAV <162917236+Tech-sunil-yadav@users.noreply.github.com>
2024-03-20 20:16:30 +0000

    DATATYPE

---
+++ 01_basics/datatype.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+// alert(3+3) // here give eeror because we are using nodejs, not broweser
+console.log(3
+    + 
+    3) //  it's not good practise -- because code readability should be high
+console.log("sunil yadav")
+
+let name = "sunil"
+let age =18
+let isLoggedin = false
+let state;
+
+// number => range 2 to power53
+// biglnt => use when need large data like trading site or facebook
+//string => ""
+// boolean +> true /false
+//null => standalone value
+//undefined  
+
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object -- intyerview related type of null is object
