e7c38cf0d98a8f17bd276334a4d75b451d1087c6
E.R S.K YADAV <162917236+Tech-sunil-yadav@users.noreply.github.com>
2024-03-22 20:49:43 +0000

    comparison_operator

---
+++ 01_basics/comparison_operator.js
@@ -0,0 +1,19 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2 ==1)
+console.log(2!=1);
+
+console.log("2" >1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log( undefined < 0);
+
+// === it is strictly checking means also checking datatypes
+console.log("2" === 2);
