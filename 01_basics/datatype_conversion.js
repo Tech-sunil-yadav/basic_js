0b146dc792d879a9d57c4f74c58c349b9c9f34de
E.R S.K YADAV <162917236+Tech-sunil-yadav@users.noreply.github.com>
2024-03-22 20:24:32 +0000

    conversion

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,30 @@
+// let score = "33abc";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let score = "Abhinay"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+// let isLoggedIn = "sunil"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
