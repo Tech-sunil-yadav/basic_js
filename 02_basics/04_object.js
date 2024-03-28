d906c6c3699e0459b8ead83e800a205ac4af6e41
E.R S.K YADAV <162917236+Tech-sunil-yadav@users.noreply.github.com>
2024-03-28 00:49:42 +0000

    OBJECT-02

---
+++ 02_basics/04_object.js
@@ -0,0 +1,54 @@
+// const tinderUser = new object() --- singleton object
+const tinderUser = {} // will give empty object
+
+tinderUser.id = "123abc"
+tinderUser.name = "sunil"
+tinderUser.isLoggedIn = false
+//console.log(tinderUser)
+const regularUser = {
+    email: "sunil.yadav@yahoo.com",
+    FullName :{
+           UserName:{
+            firstName:"Sunil",
+            lastName: "Yadav"
+           }
+    }
+}
+//console.log(regularUser.FullName.UserName.firstName)
+
+const obj1 = {1:"one",2:"two"}
+const obj2 = {3:"four",4:"five"}
+//const obj3 = {obj1,obj2} -- it will give object in object like array
+
+//const obj3 = Object.assign({},obj1,obj2) // wiil return single oject  // {}-- act as target and remaing asct as source
+
+// const obj3 = {...obj1,...obj2}  // spread operator -- most of the time we will prefer this way
+// console.log(obj3)
+
+const users = [
+    {
+        id:1,
+        email: "s@gmail.com"
+    },
+    {
+        id:2,
+        email: "a@gmail.com"
+    },
+    {
+        id:3,
+        email: "v@gmail.com"
+    },
+    {
+        id:4,
+        email: "g@gmail.com"
+    }
+
+]
+users[1].email
+console.log(tinderUser)
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); // converted every keys and values in array as divided into array behaf of key
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // check the existing whether element available or not
