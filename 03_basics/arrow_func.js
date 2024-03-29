28853dbe47e29188d3508554d24e37704626c4bc
E.R S.K YADAV <162917236+Tech-sunil-yadav@users.noreply.github.com>
2024-03-29 17:42:56 +0000

    arrowfunction

---
+++ 03_basics/arrow_func.js
@@ -0,0 +1,57 @@
+// using of this 
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+                                // this refers to context value m
+                                // in browser the global object is window
+                                // But in other engine {} is empty
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2  // implicity means no use of return 
+
+// const addTwo = (num1, num2) => ( num1 + num2 ) // if implicity call then we will prefer () not curely braces
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
