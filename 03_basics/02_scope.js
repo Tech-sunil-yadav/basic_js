b6184c1c4e7734489c8c6b639be7854c8fc53143
E.R S.K YADAV <162917236+Tech-sunil-yadav@users.noreply.github.com>
2024-03-29 17:04:19 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,59 @@
+
+//var c = 300   because of scoping issue we do not prefer var keyword for creating variable -- because even we declare variable in local scope but it acessed globally
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
